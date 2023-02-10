eed9f93c1bf45db814b08314c7963e2ccb28793f
ThisTestUser <ThisTestUser@users.noreply.github.com>
2023-02-10 15:55:30 -0500

    Undo chat log fix
    Only needed in 1.19.3

--- src/main/java/net/wurstclient/mixin/ChatHudMixin.java
+++ src/main/java/net/wurstclient/mixin/ChatHudMixin.java
@@ -54,18 +54,11 @@ public class ChatHudMixin extends DrawableHelper
 		message = event.getComponent();
 		indicator = WurstClient.INSTANCE.getOtfs().noChatReportsOtf
 			.modifyIndicator(message, signature, indicator);
-		shadow$logChatMessage(message, indicator);
 		shadow$addMessage(message, signature, client.inGameHud.getTicks(),
 			indicator, false);
 		ci.cancel();
 	}
 	
-	@Shadow
-	private void shadow$logChatMessage(Text message, @Nullable MessageIndicator indicator)
-	{
-
-	}
-
 	@Shadow
 	private void shadow$addMessage(Text message,
 		@Nullable MessageSignatureData signature, int ticks,
