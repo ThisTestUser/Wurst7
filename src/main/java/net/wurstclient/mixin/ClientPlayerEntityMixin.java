033583f0bc4b0f773a49ac552ff639da8c0c54d8
ThisTestUser <ThisTestUser@users.noreply.github.com>
2023-03-15 22:04:09 -0400

    BoatFly -> Vehicle
    -HorseJump
    -Boat control (using facing)
    -Auto swim (dolphin mode for vehicles)
    -Allow gliding and flying to be configured
    
    Note: EntityControl is now patched  by vanilla and was not included here.

--- src/main/java/net/wurstclient/mixin/ClientPlayerEntityMixin.java
+++ src/main/java/net/wurstclient/mixin/ClientPlayerEntityMixin.java
@@ -58,6 +58,8 @@ public class ClientPlayerEntityMixin extends AbstractClientPlayerEntity
 	implements IClientPlayerEntity
 {
 	@Shadow
+	private float mountJumpStrength;
+	@Shadow
 	private float lastYaw;
 	@Shadow
 	private float lastPitch;
@@ -92,6 +94,15 @@ public class ClientPlayerEntityMixin extends AbstractClientPlayerEntity
 			return false;
 		
 		return player.isUsingItem();
+	}
+	
+	@Inject(at = @At(value = "INVOKE",
+		target = "Lnet/minecraft/client/network/ClientPlayerEntity;getMountJumpStrength()F",
+		ordinal = 0), method = "tickMovement()V")
+	private void setHorseJump(CallbackInfo ci)
+	{
+		if(WurstClient.INSTANCE.getHax().vehicleHack.forceHighestJump())
+			mountJumpStrength = 1;
 	}
 	
 	@Inject(at = @At(value = "INVOKE",
