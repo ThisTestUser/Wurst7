6b60ad7f59adf364c71c2c37f55c458ff3e481a0
ThisTestUser <ThisTestUser@users.noreply.github.com>
2024-10-17 22:31:16 -0400

    MoreInv

---
+++ src/main/java/net/wurstclient/hacks/MoreInventoryHack.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2014-2024 Wurst-Imperium and contributors.
+ *
+ * This source code is subject to the terms of the GNU General Public
+ * License, version 3. If a copy of the GPL was not distributed with this
+ * file, You can obtain one at: https://www.gnu.org/licenses/gpl-3.0.txt
+ */
+package net.wurstclient.hacks;
+
+import net.minecraft.network.packet.c2s.play.CloseHandledScreenC2SPacket;
+import net.wurstclient.Category;
+import net.wurstclient.SearchTags;
+import net.wurstclient.events.PacketOutputListener;
+import net.wurstclient.hack.Hack;
+
+@SearchTags({"more inventory", "xcarry"})
+public final class MoreInventoryHack extends Hack
+	implements PacketOutputListener
+{
+	public MoreInventoryHack()
+	{
+		super("MoreInventory");
+		setCategory(Category.OTHER);
+	}
+	
+	@Override
+	protected void onEnable()
+	{
+		EVENTS.add(PacketOutputListener.class, this);
+	}
+	
+	@Override
+	protected void onDisable()
+	{
+		EVENTS.remove(PacketOutputListener.class, this);
+	}
+	
+	@Override
+	public void onSentPacket(PacketOutputEvent event)
+	{
+		if(event.getPacket() instanceof CloseHandledScreenC2SPacket)
+			event.cancel();
+	}
+}
