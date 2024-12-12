376e227a5a7584be01af624239a0f513417b715e
ThisTestUser <ThisTestUser@users.noreply.github.com>
2024-12-12 13:01:05 -0500

    NoBeaconBeam

---
+++ src/main/java/net/wurstclient/mixin/BeaconBlockEntityRendererMixin.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2014-2024 Wurst-Imperium and contributors.
+ *
+ * This source code is subject to the terms of the GNU General Public
+ * License, version 3. If a copy of the GPL was not distributed with this
+ * file, You can obtain one at: https://www.gnu.org/licenses/gpl-3.0.txt
+ */
+package net.wurstclient.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.block.entity.BeaconBlockEntityRenderer;
+import net.minecraft.client.util.math.MatrixStack;
+import net.wurstclient.WurstClient;
+
+@Mixin(BeaconBlockEntityRenderer.class)
+public class BeaconBlockEntityRendererMixin
+{
+	@Inject(at = @At("HEAD"),
+		method = "renderBeam(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;FJII[F)V",
+		cancellable = true)
+	private static void onRenderBeam(MatrixStack matrices,
+		VertexConsumerProvider vertexConsumers, float tickDelta, long worldTime,
+		int yOffset, int maxY, float[] color, CallbackInfo ci)
+	{
+		if(WurstClient.INSTANCE.getHax().noBeaconBeamHack.isEnabled())
+			ci.cancel();
+	}
+}
