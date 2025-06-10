ff9fce49c853e8a126d222ac834106c016788230
ThisTestUser <ThisTestUser@users.noreply.github.com>
2025-06-09 20:45:16 -0400

    AntiKB keepvelocity + PlayerMoveEvent changes
    PlayerMoveEvent changes are needed for ExtraElytra

--- src/main/java/net/wurstclient/hacks/AntiKnockbackHack.java
+++ src/main/java/net/wurstclient/hacks/AntiKnockbackHack.java
@@ -11,6 +11,7 @@ import net.wurstclient.Category;
 import net.wurstclient.SearchTags;
 import net.wurstclient.events.KnockbackListener;
 import net.wurstclient.hack.Hack;
+import net.wurstclient.settings.CheckboxSetting;
 import net.wurstclient.settings.SliderSetting;
 import net.wurstclient.settings.SliderSetting.ValueDisplay;
 
@@ -32,12 +33,18 @@ public final class AntiKnockbackHack extends Hack implements KnockbackListener
 				+ "100% = no knockback\n" + ">100% = reverse knockback",
 			1, -1, 2, 0.01, ValueDisplay.PERCENTAGE);
 	
+	private final CheckboxSetting keepVelocity =
+		new CheckboxSetting("Keep Velocity",
+			"Accounts for your current velocity when adjusting the knockback.",
+			false);
+	
 	public AntiKnockbackHack()
 	{
 		super("AntiKnockback");
 		setCategory(Category.COMBAT);
 		addSetting(hStrength);
 		addSetting(vStrength);
+		addSetting(keepVelocity);
 	}
 	
 	@Override
@@ -58,8 +65,22 @@ public final class AntiKnockbackHack extends Hack implements KnockbackListener
 		double verticalMultiplier = 1 - vStrength.getValue();
 		double horizontalMultiplier = 1 - hStrength.getValue();
 		
-		event.setX(event.getDefaultX() * horizontalMultiplier);
-		event.setY(event.getDefaultY() * verticalMultiplier);
-		event.setZ(event.getDefaultZ() * horizontalMultiplier);
+		if(keepVelocity.isChecked())
+		{
+			double xOffset = (event.getDefaultX() - MC.player.getVelocity().x)
+				* horizontalMultiplier;
+			double yOffset = (event.getDefaultY() - MC.player.getVelocity().y)
+				* verticalMultiplier;
+			double zOffset = (event.getDefaultZ() - MC.player.getVelocity().z)
+				* horizontalMultiplier;
+			event.setX(MC.player.getVelocity().x + xOffset);
+			event.setY(MC.player.getVelocity().y + yOffset);
+			event.setZ(MC.player.getVelocity().z + zOffset);
+		}else
+		{
+			event.setX(event.getDefaultX() * horizontalMultiplier);
+			event.setY(event.getDefaultY() * verticalMultiplier);
+			event.setZ(event.getDefaultZ() * horizontalMultiplier);
+		}
 	}
 }
