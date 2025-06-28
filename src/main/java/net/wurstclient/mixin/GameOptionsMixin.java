3d4dc3532f200c719d5142c5b630d3fa2469427e
ThisTestUser <ThisTestUser@users.noreply.github.com>
2025-06-28 17:42:17 -0400

    RenderDistance

---
+++ src/main/java/net/wurstclient/mixin/GameOptionsMixin.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2014-2025 Wurst-Imperium and contributors.
+ *
+ * This source code is subject to the terms of the GNU General Public
+ * License, version 3. If a copy of the GPL was not distributed with this
+ * file, You can obtain one at: https://www.gnu.org/licenses/gpl-3.0.txt
+ */
+package net.wurstclient.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import net.minecraft.client.option.GameOptions;
+import net.wurstclient.WurstClient;
+
+@Mixin(GameOptions.class)
+public class GameOptionsMixin
+{
+	@Inject(at = @At("HEAD"),
+		method = "getClampedViewDistance()I",
+		cancellable = true)
+	private void onGetViewDistance(CallbackInfoReturnable<Integer> cir)
+	{
+		if(WurstClient.INSTANCE.getHax().renderDistanceHack.isEnabled())
+			cir.setReturnValue(
+				WurstClient.INSTANCE.getHax().renderDistanceHack.getDistance());
+	}
+}
