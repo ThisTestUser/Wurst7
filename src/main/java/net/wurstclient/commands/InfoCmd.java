d5b6aeb890f255dc2ae87640508d73ebaa1ec728
ThisTestUser <ThisTestUser@users.noreply.github.com>
2026-04-15 15:05:55 -0400

    .info, .look, .specttp

---
+++ src/main/java/net/wurstclient/commands/InfoCmd.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright (c) 2014-2026 Wurst-Imperium and contributors.
+ *
+ * This source code is subject to the terms of the GNU General Public
+ * License, version 3. If a copy of the GPL was not distributed with this
+ * file, You can obtain one at: https://www.gnu.org/licenses/gpl-3.0.txt
+ */
+package net.wurstclient.commands;
+
+import net.minecraft.SharedConstants;
+import net.minecraft.client.multiplayer.ServerData;
+import net.wurstclient.WurstClient;
+import net.wurstclient.command.CmdError;
+import net.wurstclient.command.CmdException;
+import net.wurstclient.command.CmdSyntaxError;
+import net.wurstclient.command.Command;
+import net.wurstclient.util.ChatUtils;
+import net.wurstclient.util.LastServerRememberer;
+
+public final class InfoCmd extends Command
+{
+	public InfoCmd()
+	{
+		super("info",
+			"Gives you information about either the client or the server.",
+			".info (client|server)");
+	}
+	
+	@Override
+	public void call(String[] args) throws CmdException
+	{
+		if(args.length == 1)
+		{
+			if(args[0].equalsIgnoreCase("client"))
+			{
+				String version = "Version: " + WurstClient.MC_VERSION;
+				String protocolversion =
+					"Protocol Version: " + SharedConstants.getProtocolVersion();
+				String name = "Name: " + MC.getUser().getName();
+				String sessionid = "Session id: " + MC.getUser().getSessionId();
+				String uuid = "UUID: " + MC.player.getUUID();
+				ChatUtils.message(version);
+				ChatUtils.message(protocolversion);
+				ChatUtils.message(name);
+				ChatUtils.message(sessionid);
+				ChatUtils.message(uuid);
+			}else if(args[0].equalsIgnoreCase("server"))
+			{
+				ServerData info = LastServerRememberer.getLastServer();
+				if(info == null)
+					throw new CmdError("You haven't joined a server!");
+				String version = "Server version: " + info.version.getString();
+				String ping = "Ping: " + info.ping;
+				String list = "Players: " + info.playerList;
+				String slots = "Player Slots: " + info.status.getString();
+				String ip = "IP: " + info.ip;
+				String motd = "MOTD: " + info.motd.toString();
+				String name = "Server Name: " + info.name;
+				String brand =
+					"Server Brand: " + MC.player.connection.serverBrand();
+				String protocolversion = "Protocol version: " + info.version;
+				ChatUtils.message(version);
+				ChatUtils.message(ping);
+				ChatUtils.message(list);
+				ChatUtils.message(slots);
+				ChatUtils.message(ip);
+				ChatUtils.message(motd);
+				ChatUtils.message(name);
+				ChatUtils.message(brand);
+				ChatUtils.message(protocolversion);
+			}else
+				throw new CmdSyntaxError();
+		}else
+			throw new CmdSyntaxError();
+	}
+}
---
+++ src/main/java/net/wurstclient/commands/SpectTpCmd.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2014-2026 Wurst-Imperium and contributors.
+ *
+ * This source code is subject to the terms of the GNU General Public
+ * License, version 3. If a copy of the GPL was not distributed with this
+ * file, You can obtain one at: https://www.gnu.org/licenses/gpl-3.0.txt
+ */
+package net.wurstclient.commands;
+
+import java.util.UUID;
+
+import net.minecraft.client.multiplayer.PlayerInfo;
+import net.minecraft.network.protocol.game.ServerboundTeleportToEntityPacket;
+import net.minecraft.util.StringUtil;
+import net.wurstclient.command.CmdError;
+import net.wurstclient.command.CmdException;
+import net.wurstclient.command.CmdSyntaxError;
+import net.wurstclient.command.Command;
+import net.wurstclient.util.ChatUtils;
+
+public final class SpectTpCmd extends Command
+{
+	public SpectTpCmd()
+	{
+		super("specttp",
+			"Allows you to spectate an entity with a UUID "
+				+ "or player name. Requires spectator mode.",
+			".specttp [<entity>]");
+	}
+	
+	@Override
+	public void call(String[] args) throws CmdException
+	{
+		if(!MC.player.isSpectator())
+			throw new CmdError("Spectator mode only.");
+		if(args.length == 1)
+		{
+			if(args[0].length() == 36 && args[0].split("-").length == 5)
+			{
+				try
+				{
+					UUID uuid = UUID.fromString(args[0]);
+					MC.player.connection
+						.send(new ServerboundTeleportToEntityPacket(uuid));
+					ChatUtils.message("Spectate request sent.");
+				}catch(IllegalArgumentException e)
+				{
+					throw new CmdSyntaxError("Invalid UUID specified.");
+				}
+			}else
+			{
+				UUID uuid = null;
+				for(PlayerInfo entry : MC.player.connection.getOnlinePlayers())
+				{
+					String name = entry.getProfile().name();
+					name = StringUtil.stripColor(name);
+					
+					if(name.equals(args[0]))
+						uuid = entry.getProfile().id();
+				}
+				if(uuid == null)
+					throw new CmdError(
+						"Player \"" + args[0] + "\" could not be found.");
+				MC.player.connection
+					.send(new ServerboundTeleportToEntityPacket(uuid));
+				ChatUtils.message("Spectate request sent.");
+			}
+		}else
+			throw new CmdSyntaxError();
+	}
+}
